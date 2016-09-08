f51095087e62030f8c440b5647e79c053df8dca1
Matt Kraatz <matt.kraatz@gmail.com>
2016-09-08 14:29:03 -0500

    more firebase calls

--- app/factories/firebaseFactory.js
+++ app/factories/firebaseFactory.js
@@ -27,14 +27,68 @@ app.factory('firebaseFactory',function($q,$http,FBCreds) {
     })
   };
 
-  let pushBoard = (userID) => {};
-  let pushPin = (boardID) => {};
+  let pushBoard = (boardObj) => {
+    return $q((resolve,reject) => {
+      $http.post('{FBCreds.databaseURL}/boards',boardObj).then((boardID) => {
+        resolve(boardID)
+      }),(error) => {
+        console.error(error);
+        reject(error);
+      }
+    })
+  };
+  let pushPin = (pinObj) => {
+    return $q((resolve,reject) => {
+      $http.post('{FBCreds.databaseURL}/pins',pinObj).then((pinID) => {
+        resolve(pinID)
+      }),(error) => {
+        console.error(error);
+        reject(error);
+      }
+    })
+  };
 
-  let patchBoard = (boardID) => {};
-  let patchPin = (pinID) => {};
+  let patchBoard = (boardObj) => {
+    return $q((resolve,reject) => {
+      $http.patch('{FBCreds.databaseURL}/boards/{boardObj.boardid}',boardObj).then((boardID) => {
+        resolve(boardID)
+      }),(error) => {
+        console.error(error);
+        reject(error);
+      }
+    })
+  };
+  let patchPin = (pinObj) => {
+    return $q((resolve,reject) => {
+      $http.patch('{FBCreds.databaseURL}/pins/{pinObj.pinid}',pinObj).then((pinID) => {
+        resolve(pinID)
+      }),(error) => {
+        console.error(error);
+        reject(error);
+      }
+    })
+  };
 
-  let deleteBoard = (boardID) => {};
-  let deletePin = (pinID) => {};
+  let deleteBoard = (boardID) => {
+    return $q((resolve,reject) => {
+      $http.delete('{FBCreds.databaseURL}/boards/{boardid}').then((boardID) => {
+        resolve(boardID)
+      }),(error) => {
+        console.error(error);
+        reject(error);
+      }
+    })
+  };
+  let deletePin = (pinID) =>
+    return $q((resolve,reject) => {
+      $http.delete('{FBCreds.databaseURL}/pins/{pinid}').then((pinID) => {
+        resolve(pinID)
+      }),(error) => {
+        console.error(error);
+        reject(error);
+      }
+    })
+  };
 
   let convertResultsToArray = (object,idType,uid) => {
     let resultsArray = [];
