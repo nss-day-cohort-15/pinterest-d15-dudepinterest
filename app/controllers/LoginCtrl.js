6c4f3851aaf36ce8a3069dc766bfbcf42e08e305
Casey Reed <casey.w.reed@gmail.com>
2016-09-08 13:58:34 -0500

    Authorization completed via email

---
+++ app/controllers/LoginCtrl.js
@@ -0,0 +1,41 @@
+"use strict";
+
+app.controller("LoginCtrl", function ($scope, $window, AuthFactory) {
+
+    $scope.account = {
+        email: "",
+        password: ""
+    }
+
+    $scope.register = () => {
+        console.log("you clicked register")
+        AuthFactory.createUser({
+            email: $scope.account.email,
+            password: $scope.account.password
+        })
+        .then( (userData) => {
+            console.log("userData", userData)
+            $scope.login()
+        }, (error) => {
+            console.log(`Error creating user: ${error}`)
+        })
+    }
+
+    $scope.login = () => {
+        console.log("you clicked login")
+        AuthFactory.loginUser($scope.account)
+        .then( (data) => {
+            if (data) {
+            AuthFactory.setUid(data.uid)
+            $window.location.href = "#/boards"
+            } else {
+            $window.location.href = "#/login"
+            }
+            console.log("data from login", data)
+        })
+    }
+
+
+
+
+})
