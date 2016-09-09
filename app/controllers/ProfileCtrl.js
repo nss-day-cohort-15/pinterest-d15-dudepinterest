2abc069cc08bb2730180e26f8284a752c30311ca
Matt Kraatz <matt.kraatz@gmail.com>
2016-09-09 15:13:31 -0500

    styling boards view

--- app/controllers/ProfileCtrl.js
+++ app/controllers/ProfileCtrl.js
@@ -38,6 +38,11 @@ app.controller("ProfileCtrl", function ($scope, firebaseFactory, $routeParams, A
         // firebaseFactory.patchBoard($routeParams.boardid)
     }
 
-
+    $scope.randomColor = () => {
+        let colors = ['#f44336','#e91e63','#9c27b0','#673ab7','#3f51b5','#2196f3','#03a9f4','#00bcd4 ','#009688','#4caf50','#8bc34a',
+        '#cddc39','#ffeb3b','#ffc107','#ff9800','#ff5722','#795548','#9e9e9e','#607d8b'];
+        let colorIndex = Math.floor(Math.random() * colors.length);
+        return colors[colorIndex];
+    }
 
 })
