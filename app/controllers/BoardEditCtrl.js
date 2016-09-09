fe4e51e21afa6dc394c79c39e964ee9da105fc84
Garrett Vangilder <garrett.vangilder@gmail.com>
2016-09-09 10:57:15 -0500

    edit board

---
+++ app/controllers/BoardEditCtrl.js
@@ -0,0 +1,29 @@
+"use strict";
+
+app.controller("BoardEditCtrl", function($scope, $location, $routeParams, firebaseFactory, AuthFactory) {
+    $scope.title = "Edit Board"
+    $scope.btnText = "Update";
+    $scope.boards = [];
+    $scope.newBoard = {};
+
+
+    firebaseFactory.getBoards(AuthFactory.getUid())
+      .then( (filteredBoardArray)=> {
+        $scope.boards = filteredBoardArray;
+        $scope.selectedBoard = $scope.boards.filter(function(board) {
+          return board.boardid === $routeParams.boardid;
+        })[0];
+      });
+
+
+
+    $scope.addNewItem = () => {
+        firebaseFactory.updateSingleBoard($routeParams.boardid, $scope.selectedBoard)
+            .then((response) => {
+                $location.url("/boards")
+            });
+    };
+
+
+
+});
